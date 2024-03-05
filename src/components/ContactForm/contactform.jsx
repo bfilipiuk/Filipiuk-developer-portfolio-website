215ff957d9d9d056e60ff197a82ed3d160ec1201
Bartlomiej Filipiuk <01169277@pw.edu.pl>
2024-03-05 22:32:28 +0100

    poczatek contact formu

--- src/components/ContactForm/contactform.jsx
+++ src/components/ContactForm/contactform.jsx
@@ -3,9 +3,13 @@ import './contactform.css';
 
 const ContactForm = () => {
     return (
-        <div className="contact-form">
-            form
-        </div>
+        <form className="contact-form">
+            <div className="contact-form-header">Skontakuj się ze mną</div>
+            <input className="form-input" type="text" id="name" name="name" placeholder="Imię: " />
+            <input className="form-input" type="text" id="lastname" name="lastname" placeholder="Nazwisko: " />
+            <input className="form-input" type="text" id="email" name="email" placeholder="Email: " />
+            <textarea className="form-text-area" type="text" id="message" name="message" placeholder="Wiadomość: " /> 
+        </form>
     )
 }
 
