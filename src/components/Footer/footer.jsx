0d65ea2056b05d92d1922681d1bec839b445641d
Bartlomiej Filipiuk <01169277@pw.edu.pl>
2024-03-07 23:36:20 +0100

    aaa

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ReactComponent as GitIcon } from '../../assets/githubicon.svg';
+import { ReactComponent as LinkedinIcon } from '../../assets/LinkedIn_icon.svg';
 import './footer.css';
 
 const Footer = () => {
@@ -7,7 +9,8 @@ const Footer = () => {
             <span className="date">2024</span>
             <span className="footer-text">Made with love by Bartek Filipiuk </span>
             <div className="footer-icons">
-                tu beda ikonki
+                <a href="https://www.google.com/" target="_blank" rel="noreferrer"><LinkedinIcon className="footer-linkedin-icon" /></a>
+                <a href="https://www.google.com/" target="_blank" rel="noreferrer"><GitIcon className="footer-git-icon" /></a>
             </div>
         </div>
     );
